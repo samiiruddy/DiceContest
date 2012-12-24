5cf7ed684eab7d8f8afee8424073bcb5e0a873f0
samiiruddy <sruddy@syr.edu>
2012-12-24 14:16:13 -0500

    second commit

--- DiceContest.js
+++ DiceContest.js
@@ -8,16 +8,22 @@ var die = function(try1, try2){
 if(roll1 === true){
 	console.log("You rolled a "+try1);
 }
+else{
+	"You declined to roll!";
+}
 
 if(roll2 === true){
-console.log("You rolled a "+try2);
+	console.log("You rolled a "+try2);
+}
+else{
+	"You declined to roll!";
 }
 
 if (try1>try2){
-console.log("your first roll was higher!");
+	console.log("your first roll was higher!");
 }
 if (try1<try2){
-console.log("your second roll was higher!");
+	console.log("your second roll was higher!");
 }
 if (try1 === try2)
 {
